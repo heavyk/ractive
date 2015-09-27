fc0f94dfee463cad19349936a0c93c305d47229b
flames of love <mechanicofthesequence@gmail.com>
2015-09-27 22:52:59 +0200

    apply viewmodel mappings fix from @evs-chris (but not test)
    https://github.com/ractivejs/ractive/pull/2170#issuecomment-143593298

--- src/virtualdom/items/Component/prototype/rebind.js
+++ src/virtualdom/items/Component/prototype/rebind.js
@@ -1,6 +1,17 @@
 export default function Component$rebind ( oldKeypath, newKeypath ) {
 	var query;
 
+	const mappings = this.instance.viewmodel.mappings;
+	let m;
+	Object.keys( mappings ).forEach( k => {
+		m = mappings[k];
+		if ( m.keypath.startsWith( oldKeypath ) ) {
+			m.unbind( true );
+			m.keypath = m.keypath.replace( oldKeypath, newKeypath );
+			m.setup();
+		}
+	});
+
 	this.resolvers.forEach( rebind );
 
 	for ( let k in this.yielders ) {
